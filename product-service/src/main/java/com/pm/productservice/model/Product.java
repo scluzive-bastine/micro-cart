9db65ccf5cb3ef2ae9a64b2990ddb121d075b08a
bastinee <sabastine.nwachukwu@vehiclesinvideo.co.uk>
2025-05-31 17:25:23 +0100

    Micro cart initialization and product service added

---
+++ product-service/src/main/java/com/pm/productservice/model/Product.java
@@ -0,0 +1,118 @@
+package com.pm.productservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String description;
+
+    @Column(unique = true, nullable = false)
+    private String slug;
+
+
+    @NotNull
+    private BigDecimal price;
+
+    @NotNull
+    private String imageUrl;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    @PrePersist
+    public void generateSlug() {
+        if(this.slug == null && this.title != null) {
+            this.slug = this.title.toLowerCase()
+                    .replaceAll("[^a-zA-Z0-9]", "-")
+                    .replaceAll("^-+|-+$", "");
+        }
+    }
+
+}
