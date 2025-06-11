ebbaa5b82a773255ab9ebacf3b7a65b915e2fe75
scluzive-bastine <toshfuture@gmail.com>
2025-06-11 16:08:39 +0100

    Clients integration tests added

---
+++ integration-tests/src/test/java/UserIntegrationTest.java
@@ -0,0 +1,46 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class UserIntegrationTest {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnUserWithValidToken() {
+
+        String loginPayload = """
+                {
+                    "email": "testuser@test.com",
+                    "password": "password123"
+                }
+                """;
+
+
+        String token = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("token");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/clients")
+                .then()
+                .statusCode(200)
+                .body("clients", notNullValue());
+
+    }
+}
